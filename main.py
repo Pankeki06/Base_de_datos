f96f6d735ced92bb5e38b6a7dcdcdd5e36be8950
Mejo <manucristrix@gmail.com>
2026-04-15 07:30:57 -0600

    bugsito en la terminal al cerrar el programa corregido xd

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import flet as ft
 from views.login_view import create_login_view
 
 
-
 def main(page: ft.Page) -> None:
     page.title = "Gestión de seguros"
     page.window_width = 900
@@ -12,5 +11,9 @@ def main(page: ft.Page) -> None:
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.add(create_login_view(page))
 
+
 if __name__ == "__main__":
-    ft.run(main)
+    try:
+        ft.run(main)
+    except KeyboardInterrupt:
+        print("Aplicación cerrada correctamente")
